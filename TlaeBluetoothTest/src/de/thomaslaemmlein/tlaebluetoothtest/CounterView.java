4bb1f02ee47a72ac73ddf2b56672546b38ed8773
tlaemmlein <thomaslaemmlein@gmail.com>
2013-01-12 23:59:13 +0100

    ADD: Initial checkin.

---
+++ TlaeBluetoothTest/src/de/thomaslaemmlein/tlaebluetoothtest/CounterView.java
@@ -0,0 +1,101 @@
+package de.thomaslaemmlein.tlaebluetoothtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class CounterView extends LinearLayout {
+	TextView m_CounterTextView;
+	
+	private int m_CurrentNumber;
+	
+	private int m_MaximalNumber = 99;
+
+	public CounterView(Context context) {
+		super(context);
+		init();
+	}
+
+	public CounterView(Context context, AttributeSet attr) 
+	{
+		super(context, attr);
+		init();
+	}
+	
+	private void init()
+	{
+		Log.d("CounterView", "init");
+		
+		LayoutInflater inflater = LayoutInflater.from(getContext());
+		View root = inflater.inflate(R.layout.counter_view, this, false);
+		
+		m_CurrentNumber = 0;
+		
+	    m_CounterTextView = (TextView)root.findViewById(R.id.counterTextView);
+	    
+	    m_CounterTextView.setText( Integer.toString(m_CurrentNumber));
+	    
+	    addView(root);
+	}
+		
+	@Override
+	 public boolean onTouchEvent(MotionEvent event) {
+		Log.d("CounterView", "onTouchEvent");
+		gestureDetector.onTouchEvent(event);
+	    return true;
+	 }
+	
+	SimpleOnGestureListener simpleOnGestureListener
+	   = new SimpleOnGestureListener(){
+
+
+	  @Override
+	  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+	    float velocityY) {
+	   String swipe = "";
+	   float sensitvity = 50;
+	   
+	   // TODO Auto-generated method stub
+	   if((e1.getX() - e2.getX()) > sensitvity){
+	    swipe += "Swipe Left;";
+	   }else if((e2.getX() - e1.getX()) > sensitvity){
+	    swipe += "Swipe Right;";
+	   }else{
+	    swipe += ";";
+	   }
+	   
+	   if((e1.getY() - e2.getY()) > sensitvity){
+	    swipe += "Swipe Up;";
+	    if (m_CurrentNumber < m_MaximalNumber)
+	    {
+		    m_CurrentNumber++;
+		    m_CounterTextView.setText( Integer.toString(m_CurrentNumber));  	
+	    }
+	   }else if((e2.getY() - e1.getY()) > sensitvity){
+		   if (m_CurrentNumber > 0)
+		   {
+			   m_CurrentNumber--;
+			   m_CounterTextView.setText( Integer.toString(m_CurrentNumber));
+		   }
+	    swipe += "Swipe Down;";
+	   }else{
+	    swipe += ";";
+	   }
+	   
+	   Log.d("onFling", swipe);
+	   
+	   return super.onFling(e1, e2, velocityX, velocityY);
+	  }
+	   };
+	  
+	   GestureDetector gestureDetector
+	   = new GestureDetector(getContext(), simpleOnGestureListener);
+	  
+}
---
+++ TlaeBluetoothTest/src/de/thomaslaemmlein/tlaebluetoothtest/TlaeBluetoothTest.java
@@ -0,0 +1,128 @@
+package de.thomaslaemmlein.tlaebluetoothtest;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class TlaeBluetoothTest extends Activity {
+	
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tlae_bluetooth_test);
+        // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+		
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+        // If BT is not on, request that it be enabled.
+        // onActivityResult will be called
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        } 		
+	}
+	
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_tlae_bluetooth_test, menu);
+		return true;
+	}
+	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent serverIntent = null;
+        switch (item.getItemId()) {
+        case R.id.secure_connect_scan:
+            // Launch the DeviceListActivity to see devices and do scan
+            serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+            return true;
+        case R.id.insecure_connect_scan:
+            // Launch the DeviceListActivity to see devices and do scan
+            serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+            return true;
+        case R.id.discoverable:
+            // Ensure this device is discoverable by others
+            ensureDiscoverable();
+            return true;
+        }
+        return false;
+    }
+	
+	
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case REQUEST_CONNECT_DEVICE_SECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, true);
+            }
+            break;
+        case REQUEST_CONNECT_DEVICE_INSECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, false);
+            }
+            break;        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+                // Bluetooth is now enabled, so set up a chat session
+                Toast.makeText(this, "Bluetooth is now enabled, so set up a chat session", Toast.LENGTH_SHORT).show();
+            } else {
+                // User did not enable Bluetooth or an error occured
+                Toast.makeText(this, "User did not enable Bluetooth or an error occured", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras()
+            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BLuetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        Toast.makeText(this, "Attempt to connect to the device", Toast.LENGTH_SHORT).show();
+        //mChatService.connect(device, secure);
+    }
+
+	
+
+}
